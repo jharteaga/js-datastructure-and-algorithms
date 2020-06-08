df30a6344c36838ab6c77d71dc08bd8457626c9f
jharteaga <jh.arteaga09@gmail.com>
2020-06-07 23:57:04 -0700

    MaxChar problem solved

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const counts = {};
+  let max = '';
+  let maxCount = 0;
+
+  for (let char of str) counts[char] = counts[char] + 1 || 1;
+
+  for (let char in counts)
+    if (counts[char] > maxCount) {
+      maxCount = counts[char];
+      max = char;
+    }
+
+  return max;
+}
 
 module.exports = maxChar;
